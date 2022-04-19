22926f95f95c9f0dbb1e43068cc857d4e7202d35
Surender Siwach <54826291+SSiwach@users.noreply.github.com>
2022-04-20 00:00:22 +0530

    Create cross_entropy_loss_fun.py

---
+++ cross_entropy_loss_fun.py
@@ -0,0 +1,28 @@
+class Loss: 
+  def calculate(self, output, y):
+    sample_losses = self.forward(output,y)
+    data_loss = np.mean(sample_losses)
+    return data_loss
+
+class Loss_CategoricalCrossentropy(Loss):
+  def forward(self, y_pred, y_true):
+    samples = len(y_pred)
+    y_pred_clipped = np.clip(y_pred, 1e-7, 1 - 11e-7)
+
+    if len(y_true.shape) == 1:
+      correct_confidences = y_pred_clipped[
+                                           range(samples),
+                                           y_true
+      ]
+    elif len(y_true.shape)==2:
+      correct_confidences = np.sum(
+          y_pred_clipped*y_true,
+          axis =1 
+      )
+
+    negative_log_likehoods = -np.log(correct_confidences)
+    return negative_log_likehoods
+
+loss_function = Loss_CategoricalCrossentropy()
+loss = loss_function.calculate(softmax_outputs, class_targets)
+print(loss)
