7c30aa3005d63504dd9fe6108a2a91ee01e24b91
Surender Siwach <54826291+SSiwach@users.noreply.github.com>
2022-08-01 21:35:38 +0530

    Update input_layer.py

--- ML_from_scratch/input_layer.py
+++ ML_from_scratch/input_layer.py
@@ -1,33 +1,19 @@
-# Dropout
-class Layer_Dropout:
-
-  #initialize 
-  def __init__(self, rate):
-    #store rate , we invert it as for example for dropout of 0.1 we need succes rate  0.9
-
-    self.rate = 1 - rate
-  
+class Layer_Input :
   #forward pass
+
   def forward(self, inputs, training):
-
-    #save input values
+    #remember input values 
     self.inputs = inputs
+    #calculate output values from inputs
+    self.output = np.maximum(0, inputs)
 
-    # if not in the training mode - return values
-    if not training:
-      self.output = inputs.copy()
-      return
-
-    #generate and save scaled mask
-    self.binary_mask =  np.random.binomial(1, self.rate,
-                                           size = inputs.shape)/self.rate
-    
-
-    #Apply mask to output values
-    self.output = inputs*self.binary_mask
-#backward pass
+  #Backward pass
   def backward(self, dvalues):
-    #Gradient on values
-    self.dinputs = dvalues * self.binary_mask
+    #as we need to modify original variable, we nedd to copy
+    self.dinputs = dvalues.copy()
 
-                                
+    #zero gradeint where i/p were negative
+    self.dinputs[self.inputs <= 0] = 0
+
+  def predictions(self, outputs):
+    return outputs
